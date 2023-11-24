e7fbcb25a1cb2a16543a9925efdbbcac145ca538
Viggo Sörensen <142979357+ViggoGitHub@users.noreply.github.com>
2023-11-24 11:08:13 +0100

    Add files via upload

---
+++ metronom.py
@@ -0,0 +1,31 @@
+# from playsound import playsound
+
+# playsound("C:\Users\viggo.sorensen1\Desktop\Python\Metronom\click_button.mp3")
+
+# import pygame
+
+# pygame.init()
+# pygame.mixer.music.load(
+#     "C:\\Users\\viggo.sorensen1\\Desktop\\Python\\Metronom\\click_button.mp3"
+# )
+# pygame.mixer.music.play()
+
+# pygame.event.wait()
+
+# import simpleaudio as sa
+
+# wave_obj = sa.WaveObject.from_wave_file(
+#     "Users\\viggo.sorensen1\\Desktop\\Python\\Metronom\\click_button.mp3"
+# )
+# play_obj = wave_obj.play()
+
+# play_obj.wait_done()
+
+
+from pydub import AudioSegment
+from pydub.playback import play
+
+sound = AudioSegment.from_file(
+    "C:\\Users\\viggo.sorensen1\\Desktop\\Python\\Metronom\\click_button.mp3"
+)
+play(sound)
