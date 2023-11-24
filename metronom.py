0428e2ce84cac015bd5b5f58aa5ab5905f222ddd
ViggoGitHub <viggoks04@gmail.com>
2023-11-24 15:06:04 +0100

    Added some buttons and text to the metronome and practiced some more on tkinter

--- metronom.py
+++ metronom.py
@@ -7,6 +7,22 @@ root = tk.Tk()
 root.geometry("900x500")
 root.title("Metronome")
 
+label = tk.Label(root, text="Metronome", font=("Arial", 18))
+# padx och pady är padding för x och y axel
+label.pack(padx=100, pady=20)
+
+label2 = tk.Label(root, text="BPM", font=("Arial", 10))
+label2.pack()
+
+entry = tk.Entry(root)
+entry.pack()
+
+start = tk.Button(root, text="Start")
+start.pack(pady=10)
+
+stop = tk.Button(root, text="Stop")
+stop.pack(pady=10)
+
 # to open a window
 root.mainloop()
 
--- practicecode.py
+++ practicecode.py
@@ -41,7 +41,34 @@ root.geometry("900x500")
 root.title("Metronome")
 
 label = tk.Label(root, text="Hej", font=("Arial", 18))
-label.pack()
+# padx och pady är padding för x och y axel
+label.pack(padx=100, pady=20)
+
+# textbox för om användaren vill skriva in någonting
+textbox = tk.Text(root, height=3, font=("Arial", 16))
+textbox.pack()
+
+# entry för mindre rutor där man bara skriver in något enstaka på en rad
+entry = tk.Entry(root)
+entry.pack()
+
+buttonframe = tk.Frame(root)
+# weight används för att knapparna ska stretcha över hela spannet och inte lämna mellanrum
+buttonframe.columnconfigure(0, weight=1)
+buttonframe.columnconfigure(1, weight=1)
+
+button = tk.Button(root, text="Klicka här!", font=("Arial", 16))
+button.pack()
+
+btn1 = tk.Button(buttonframe, text="1", font=("Arial", 16))
+btn1.grid(row=0, column=0, sticky=tk.W + tk.E)
+
+btn2 = tk.Button(buttonframe, text="2", font=("Arial", 16))
+btn2.grid(row=0, column=1, sticky=tk.W + tk.E)
+
+# fill="x" fyller i x riktning
+buttonframe.pack(fil="x")
+
 
 # to open a window
 root.mainloop()
